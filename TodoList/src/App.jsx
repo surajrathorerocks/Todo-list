6056279b12ec863af56ccd8e82217b17f18fe4d9
Suraj <131504529+surajrathorerocks@users.noreply.github.com>
2026-03-27 07:07:20 +0000

    buggy

---
+++ TodoList/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState,useEffect} from 'react'
+import './App.css'
+import { TodoProvider } from './context'
+import { TodoForm, TodoItem } from './assets/components'
+
+function App() {
+  const [Todos, setTodos] = useState([])
+  const addTodo = (Todo) =>{
+    setTodos((oldTodo)=>[{id: Date.now(),...Todo},...oldTodo])
+  }
+  const updatedTodo = (id,Todo) =>{
+    setTodos((oldTodo) => oldTodo.map((prevTodo) => (prevTodo.id === id ? Todo : prevTodo)))
+  }
+  const deleteTodo = (id) => {
+    setTodos((oldTodo) => oldTodo.filter((Todo) => Todo.id != id))
+  }
+  const toggleComplete = (id) =>{
+    setTodos((oldTodo) => oldTodo.map((prev) => prev === id ?{...prev,completed: ! prev.completed}:prev))
+  }
+  useEffect (()=>{
+  const Todos = JSON.parse(ocalStorage.getItem("Todos"))
+  if(Todos && Todos.length>0){
+    setTodos(Todos)
+  }
+  },[])
+  useEffect (()=>{
+    localStorage.setItem("Todos",JSON.stringify(Todos))
+  },[Todos])
+
+  return (
+    <TodoProvider value={{Todos,addTodo,updatedTodo,toggleComplete,deleteTodo}}>
+     <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm/>
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {Todos.map((Todo)=>(
+                         <div key={Todo.id} className='w-full'>
+                          <TodoItem Todo={Todo}/>
+                         </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+     </TodoProvider>
+  )   
+}
+
+export default App
---
+++ TodoList/src/assets/components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useTodo } from "../../context/TodoContext";
+
+function TodoForm() {
+    const[Todo,setTodo] = useState("")
+    const {addTodo} = useTodo()
+    const add = (e) => {
+        e.preventDefault()
+        if (!Todo) return 
+        addTodo({Todo, Completed:false})
+        setTodo("")
+    }
+
+    return (
+        <form onSubmit={add} className="flex">
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={Todo}
+                onChange={(e) => setTodo(e.target.value)}
+            />
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
+
